5170f643e0624701cd0a9210c66fac00d046cb57
rk10roneey <79647222+rk10roneey@users.noreply.github.com>
2021-03-01 22:01:16 +0530

    Add files via upload

---
+++ p8_jpgm.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import time
+import numpy as np
+import pandas as pd
+
+nav=st.sidebar.radio("Navigation",["Members","Registration"])
+#rad =st.sidebar.radio("Navigation",["Home","About Us"])
+# if nav=="Home":
+#     st.write("Welcome.....")
+if nav=="Members":
+    st.header("Jalpaiguri Mariners")
+    showdata= pd.read_csv("F:/Study/Data Sci/Small Project//members.csv")
+    sd=st.radio("Show Data",["Show","Hide"])
+    if sd=="Hide":
+        st.write("")
+    else:
+        st.table(showdata)
+elif nav=="Registration":
+    st.header("Jalpaiguri Mariners")
+    st.write("Registration Form")
+    first,last=st.beta_columns(2)
+    email,mob=st.beta_columns([2,1])
+    user,pw,pw2=st.beta_columns(3)
+    dob,blood,occ=st.beta_columns(3)
+    #sub=st.beta_columns(1)
+
+    first=first.text_input("First Name")
+    last=last.text_input("Last Name")
+    email=email.text_input("Email Name")
+    mob=mob.text_input("Mobile Name")
+    dob = dob.date_input("DOB")
+    blood = blood.selectbox("Blood group",["A+","A-","B+","B-","AB+","AB-","O+","O-"])
+    occ = occ.text_input("Occupation")
+    add=st.text_area("Address")
+    # user=user.text_input("UserId")
+    # pw=pw.text_input("Password",type="password")
+    # pw2=pw2.text_input("Confirm Password",type="password")
+
+    ch=st.checkbox("I Agree",value=False)
+    if ch==True:
+        st.warning("""Hereby I agree to abide by the present and future rules of the
+        Jalpaiguri Mariners and the only identity I have at the club premises, is "MOHUNBAGANI". """)
+
+        if st.button("Submit Button"):
+            to_add={"First":[first],"Last":[last],"Email":[email],"Mobile":[mob],"DOB":[dob],
+            "Blood":[blood],"Occupation":[occ],"Address":[add]}
+            to_add=pd.DataFrame(to_add)
+            to_add.to_csv("F:/Study/Data Sci/Small Project//members.csv",mode="a",header=False,index=True)
+            st.success("Submitted")
+            st.balloons()
